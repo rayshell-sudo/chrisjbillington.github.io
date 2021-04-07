cc24f8c16d906a3945175d733c918566176f41f7
chrisjbillington <chrisjbillington@gmail.com>
2021-04-07 21:21:19 +1000

    Cleanup of vax code
    before attempting to make a plot of utilisation

--- aus_vax.py
+++ aus_vax.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from datetime import datetime
-from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.units as munits
@@ -16,6 +15,7 @@ munits.registry[np.datetime64] = converter
 munits.registry[datetime.date] = converter
 munits.registry[datetime] = converter
 
+
 def n_day_average(data, n=14):
     ret = np.cumsum(data, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
@@ -24,43 +24,11 @@ def n_day_average(data, n=14):
 
 STATES = ['aus', 'nsw', 'vic', 'sa', 'wa', 'tas', 'qld', 'nt', 'act']
 
-state = 'aus'
-
-def partial_derivatives(function, x, params, u_params):
-    model_at_center = function(x, *params)
-    partial_derivatives = []
-    for i, (param, u_param) in enumerate(zip(params, u_params)):
-        d_param = u_param / 1e6
-        params_with_partial_differential = np.zeros(len(params))
-        params_with_partial_differential[:] = params[:]
-        params_with_partial_differential[i] = param + d_param
-        model_at_partial_differential = function(x, *params_with_partial_differential)
-        partial_derivative = (model_at_partial_differential - model_at_center) / d_param
-        partial_derivatives.append(partial_derivative)
-    return partial_derivatives
-
-
-def model_uncertainty(function, x, params, covariance):
-    u_params = [np.sqrt(abs(covariance[i, i])) for i in range(len(params))]
-    derivs = partial_derivatives(function, x, params, u_params)
-    squared_model_uncertainty = sum(
-        derivs[i] * derivs[j] * covariance[i, j]
-        for i in range(len(params))
-        for j in range(len(params))
-    )
-    return np.sqrt(squared_model_uncertainty)
-
-
-def quadratic(x, a, b, c):
-    return a * x ** 2 + b * x + c
-
-
-def linear(x, a, b):
-    return a * x + b
 
 def gaussian_smoothing(data, pts):
     """gaussian smooth an array by given number of points"""
     from scipy.signal import convolve
+
     x = np.arange(-4 * pts, 4 * pts + 1, 1)
     kernel = np.exp(-(x ** 2) / (2 * pts ** 2))
     normalisation = convolve(np.ones_like(data), kernel, mode='same')
@@ -77,45 +45,21 @@ for s in STATES:
     df = pd.read_html(f"https://covidlive.com.au/report/daily-vaccinations/{s}")[1]
     dates = np.array(df['DATE'][::-1])
     state_doses = np.array(df['DOSES'][::-1])
-    dates = np.array([np.datetime64(datetime.strptime(d, '%d %b %y'), 'D') for d in dates])
+    dates = np.array(
+        [np.datetime64(datetime.strptime(d, '%d %b %y'), 'D') for d in dates]
+    )
     state_doses = state_doses[dates >= START_DATE]
     dates = dates[dates >= START_DATE]
     doses_by_state[s] = state_doses
 
-# Move back some late-announced data to the dates which it applies to:
-# known_national_dailies = [
-#     ['2021-04-01', 73979],
-#     ['2021-04-02', 79283],
-#     ['2021-04-03', 4435],
-# ]
-# for date, daily_doses in known_national_dailies:
-#     i = np.argwhere(dates == np.datetime64(date))[0, 0]
-#     doses_by_state['aus'][i] = doses_by_state['aus'][i - 1] + daily_doses
 
 doses_by_state['fed'] = doses_by_state['aus'] - sum(
     doses_by_state[s] for s in STATES if s != 'aus'
 )
 
 
-doses = doses_by_state[state]
+doses = doses_by_state['aus']
 
-# dates = dates[:-1]
-# doses = doses[:-1]
-
-# doses = np.append(doses, [doses[-1] + 85000])
-# dates = np.append(dates, [dates[-1] + 1])
-# doses = np.append(doses, [doses[-1] + 85000])
-# dates = np.append(dates, [dates[-1] + 1])
-
-# doses = np.append(doses, [doses[-1] + 90000])
-# dates = np.append(dates, [dates[-1] + 1])
-
-# doses = np.append(doses, [doses[-1] + 100000])
-# dates = np.append(dates, [dates[-1] + 1])
-
-smoothed_doses = gaussian_smoothing(np.diff(doses, prepend=0), 2).cumsum()
-
-# doses = n_day_average(np.diff(doses, prepend=0), 7).cumsum()
 
 N_DAYS_PROJECT = 250
 
@@ -125,13 +69,8 @@ days_model = np.linspace(days[0], days[-1] + N_DAYS_PROJECT, 1000)
 fig1 = plt.figure(figsize=(8, 6))
 
 plt.fill_between(
-        dates + 1,
-        doses / 1e6,
-        label='Cumulative doses',
-        step='pre',
-        color='C0',
-        zorder=10
-    )
+    dates + 1, doses / 1e6, label='Cumulative doses', step='pre', color='C0', zorder=10
+)
 
 ax1 = plt.gca()
 target = 160000 * days_model
@@ -141,28 +80,6 @@ plt.plot(
     'k--',
     label='Target',
 )
-
-popt, pcov = curve_fit(quadratic, days, smoothed_doses)
-vaccinated_model = quadratic(days_model, *popt)
-u_vaccinated_model = model_uncertainty(quadratic, days_model, popt, pcov)
-
-if TREND:
-    plt.plot(
-        days_model + dates[0].astype(int),
-        vaccinated_model / 1e6,
-        color='k',
-        alpha=0.5,
-        label='Quadratic trend',
-    )
-    plt.fill_between(
-        days_model + dates[0].astype(int),
-        (vaccinated_model + 2 * u_vaccinated_model) / 1e6,
-        (vaccinated_model - 2 * u_vaccinated_model) / 1e6,
-        alpha=0.3,
-        color='k',
-        # edgecolor='grey',
-        linewidth=0,
-    )
 
 
 plt.axis(
@@ -174,58 +91,9 @@ plt.axis(
 
 plt.title(f'AUS cumulative doses. Total to date: {doses[-1]/1e3:.1f}k')
 plt.ylabel('Cumulative doses (millions)')
-# plt.yscale('log')
 
 
 fig2 = plt.figure(figsize=(8, 6))
-daily_doses = np.diff(smoothed_doses, prepend=0)
-
-plt.fill_between(
-        dates + 1,
-        daily_doses / 1000,
-        label='Daily doses (smoothed)',
-        step='pre',
-        color='C0',
-        zorder=10,
-    )
-
-# plt.bar(
-#     dates, daily_doses / 1000, width=1.0, label='Daily doses (smoothed)'
-# )
-ax2 = plt.gca()
-plt.title(
-    f'{state.upper()} daily doses. Latest rate: {daily_doses[-1] / 1000:.1f}k per day'
-)
-plt.axhline(160, color='k', linestyle='--', label="Target")
-
-if TREND:
-    dt = days_model[1] - days_model[0]
-    plt.plot(
-        days_model + dates[0].astype(int),
-        np.diff(vaccinated_model, prepend=0) / dt / 1000,
-        color='k',
-        label="Trend"
-    )
-
-    plt.fill_between(
-        days_model + dates[0].astype(int),
-        np.diff(vaccinated_model + 2 * u_vaccinated_model, prepend=0) / dt / 1000,
-        np.diff(vaccinated_model - 2 * u_vaccinated_model, prepend=0) / dt / 1000,
-        alpha=0.3,
-        color='k',
-        # edgecolor='grey',
-        linewidth=0,
-    )
-
-plt.axis(
-    xmin=dates[0].astype(int) + 1,
-    xmax=dates[0].astype(int) + 250,
-    ymin=0,
-    ymax=210,
-)
-plt.ylabel('Daily doses (thousands)')
-
-fig3 = plt.figure(figsize=(8, 6))
 
 MOST_RECENT_FED_UPDATE = np.datetime64('2021-04-07')
 FED_CLIP = len(dates) - 1 - np.argwhere(dates == MOST_RECENT_FED_UPDATE)[0, 0]
@@ -235,11 +103,13 @@ colours = list(reversed([f'C{i}' for i in range(9)]))
 for i, state in enumerate(['nt', 'act', 'tas', 'sa', 'wa', 'qld', 'vic', 'nsw', 'fed']):
     doses = doses_by_state[state]
     if state == 'fed' and FED_CLIP:
-        smoothed_doses = gaussian_smoothing(np.diff(doses[:-FED_CLIP], prepend=0), 2).cumsum()
+        smoothed_doses = gaussian_smoothing(
+            np.diff(doses[:-FED_CLIP], prepend=0), 2
+        ).cumsum()
         smoothed_doses = np.append(smoothed_doses, [smoothed_doses[-1]] * FED_CLIP)
     else:
         smoothed_doses = gaussian_smoothing(np.diff(doses, prepend=0), 2).cumsum()
-    
+
     daily_doses = np.diff(smoothed_doses, prepend=0)
     if state == 'fed' and FED_CLIP:
         latest_daily_doses = daily_doses[-FED_CLIP - 1]
@@ -281,11 +151,11 @@ plt.title(
 )
 if FED_CLIP:
     text = plt.figtext(
-            0.575,
-            0.85,
-            "* Includes projected federally-administered doses",
-            fontsize='x-small',
-        )
+        0.575,
+        0.85,
+        "* Includes projected federally-administered doses",
+        fontsize='x-small',
+    )
     text.set_bbox(dict(facecolor='white', alpha=0.8, linewidth=0))
 
 plt.ylabel('Daily doses (thousands)')
@@ -297,9 +167,9 @@ plt.axis(
     ymin=0,
     ymax=200,
 )
-ax3 = plt.gca()
+ax2 = plt.gca()
 
-for ax in [ax1, ax2, ax3]:
+for ax in [ax1, ax2]:
     ax.fill_betweenx(
         [0, 1e9],
         2 * [START_DATE.astype(int)],
@@ -331,15 +201,6 @@ for ax in [ax1, ax2, ax3]:
         )
 
 
-# plt.gca().tick_params(axis='x', rotation=90)
-# plt.axis(
-#     xmin=dates[0].astype(int),
-#     xmax=dates[-1].astype(int),
-#     ymin=0,
-#     ymax=30000,
-# )
-
-
 handles, labels = ax1.get_legend_handles_labels()
 order = [0, 1, 4, 3, 2] if TREND else [1, 0, 2, 3]
 ax1.legend(
@@ -349,42 +210,14 @@ ax1.legend(
     ncol=2,
 )
 
-handles, labels = ax2.get_legend_handles_labels()
-order = [0, 1, 4, 3, 2] if TREND else [1, 0, 2, 3]
-ax2.legend(
-    [handles[idx] for idx in order],
-    [labels[idx] for idx in order],
-    loc='upper left',
-    ncol=2,
-)
-
-for ax in [ax1, ax2, ax3]:
+for ax in [ax1, ax2]:
     ax.xaxis.set_major_locator(locator)
     ax.xaxis.set_major_formatter(formatter)
     ax.get_xaxis().get_major_formatter().show_offset = False
     ax.grid(True, linestyle=":", color='k')
-    # ax.tick_params(axis='x', rotation=90)
 
 
-ax3.legend(loc='lower right')
-
-# plt.figure()
-
-# plt.plot(dates[2:], 100 * (smoothed_doses[2:] / smoothed_doses[1:-1] - 1))
-# plt.grid(True, linestyle=':', color='k', alpha=0.5)
-# plt.axis(ymin=0, ymax=100)
-# plt.ylabel('growth rate of doses (% / day)')
-# plt.gca().xaxis.set_major_locator(mdates.DayLocator([1]))
-# plt.gca().get_xaxis().get_major_formatter().show_offset = False
-# plt.axis(
-#     xmin=dates[0].astype(int),
-#     xmax=dates[0].astype(int) + 250,
-# )
-
-
-
-
-
+ax2.legend(loc='lower right')
 
 # Update the date in the HTML
 html_file = 'aus_vaccinations.html'
@@ -396,11 +229,9 @@ for i, line in enumerate(html_lines):
 Path(html_file).write_text('\n'.join(html_lines) + '\n')
 
 fig1.savefig('cumulative_doses.svg')
-fig2.savefig('daily_doses.svg')
-fig3.savefig('daily_doses_by_state.svg')
+fig2.savefig('daily_doses_by_state.svg')
 
 fig1.savefig('cumulative_doses.png')
-fig2.savefig('daily_doses.png')
-fig3.savefig('daily_doses_by_state.png')
+fig2.savefig('daily_doses_by_state.png')
 
 plt.show()
