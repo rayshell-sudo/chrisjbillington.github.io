ba30e958622220df676b3d37e23e9d29e546b6ef
chrisjbillington <chrisjbillington@gmail.com>
2021-04-08 20:30:13 +1000

    Add vaccine utilisation plot

--- aus_vax.py
+++ aus_vax.py
@@ -8,9 +8,6 @@ from pathlib import Path
 from pytz import timezone
 
 converter = mdates.ConciseDateConverter()
-locator = mdates.DayLocator([1])
-formatter = mdates.ConciseDateFormatter(locator)
-
 munits.registry[np.datetime64] = converter
 munits.registry[datetime.date] = converter
 munits.registry[datetime] = converter
@@ -86,7 +83,7 @@ def unpack_data(s):
             date, value = line.split()
             dates.append(np.datetime64(date))
             values.append(float(value))
-    return np.array(dates), np.array(values)
+    return np.array(dates) - 4, np.array(values)
 
 pfizer_supply_dates, pfizer_supply = unpack_data(pfizer_supply_data)
 AZ_OS_supply_dates, AZ_OS_suppy = unpack_data(AZ_OS_supply_data)
@@ -97,15 +94,52 @@ AZ_shipments = np.diff(AZ_OS_suppy, prepend=0)
 AZ_production = np.diff(AZ_local_supply, prepend=0)
 
 # Calculate vaccine utilisation:
-
 first_doses = doses.astype(float)
+second_doses = np.zeros_like(first_doses)
 reserved = np.zeros_like(first_doses)
-available = np.zeros_like(reserved)
+pfizer_available = np.zeros_like(reserved)
+AZ_available = np.zeros_like(reserved)
 
-available[0] = pfizer_shipments[0]
+tau_AZ = 84
+tau_pfizer = 21
+
+pfizer_available += pfizer_shipments[pfizer_supply_dates < dates[0]].sum()
+AZ_available += AZ_shipments[AZ_OS_supply_dates < dates[0]].sum()
+AZ_available += AZ_production[AZ_local_supply_dates < dates[0]].sum()
+
 for i, date in enumerate(dates):
-    pass
+    if date in pfizer_supply_dates:
+        pfizer_available[i:] += pfizer_shipments[pfizer_supply_dates == date][0]
+    if date in AZ_OS_supply_dates:
+        AZ_available[i:] += AZ_shipments[AZ_OS_supply_dates == date][0]
+    if date in AZ_local_supply_dates:
+        AZ_available[i:] += AZ_production[AZ_local_supply_dates == date][0]
+    if i:
+        first_doses_today = first_doses[i] - first_doses[i-1]
+    else:
+        first_doses_today = first_doses[i]
 
+    AZ_frac = AZ_available[i] / (AZ_available[i] + pfizer_available[i])
+    pfizer_frac = pfizer_available[i] / (AZ_available[i] + pfizer_available[i])
+
+    AZ_first_doses_today = AZ_frac * first_doses_today
+    pfizer_first_doses_today = pfizer_frac * first_doses_today
+
+    AZ_available[i:] -= 2 * AZ_first_doses_today
+    pfizer_available[i:] -= 2 * pfizer_first_doses_today
+
+    reserved[i : i + tau_AZ] += AZ_first_doses_today
+    reserved[i : i + tau_pfizer] += pfizer_first_doses_today
+
+    first_doses[i + tau_AZ:] -= AZ_first_doses_today
+    first_doses[i + tau_pfizer:] -= pfizer_first_doses_today
+
+    second_doses[i + tau_AZ:] += AZ_first_doses_today
+    second_doses[i + tau_pfizer:] += pfizer_first_doses_today
+
+
+# plt.plot(dates, AZ_available + pfizer_available)
+# plt.show()
 
 N_DAYS_PROJECT = 250
 
@@ -216,12 +250,42 @@ plt.axis(
 ax2 = plt.gca()
 
 
-# fig3 = plt.figure(figsize=(8, 6))
+fig3 = plt.figure(figsize=(8, 6))
+cumsum = np.zeros(len(dates))
+for arr, label, colour in [
+    (first_doses, 'First doses', 'C0'),
+    (second_doses, 'Second doses', 'C1'),
+    (reserved, 'Reserved', 'C3'),
+    (pfizer_available + AZ_available, 'Available', 'C2'),
+]:
+    plt.fill_between(
+        dates + 1,
+        cumsum / 1e3,
+        (cumsum + arr) / 1e3,
+        label=f'{label} ({arr[-1] / 1000:.0f}k doses)',
+        step='pre',
+        color=colour,
+        zorder=10,
+        linewidth=0,
+    )
+    cumsum += arr
+
+used = first_doses[-1] + second_doses[-1] + reserved[-1]
+unused = AZ_available[-1] + pfizer_available[-1]
+utilisation = 100 * used / (used + unused)
+plt.ylabel('Cumulative doses (thousands)')
+plt.title(f"Estimated vaccine utilisation: latest utilisation rate: {utilisation:.1f}%")
+plt.axis(
+    xmin=dates[0].astype(int) + 1,
+    xmax=dates[0].astype(int) + 125,
+    ymin=0,
+    ymax=5000,
+)
+ax3 = plt.gca()
 
 
 
-
-for ax in [ax1, ax2]:
+for ax in [ax1, ax2, ax3]:
     ax.fill_betweenx(
         [0, ax.get_ylim()[1]],
         2 * [START_DATE.astype(int)],
@@ -258,18 +322,37 @@ order = [1, 0, 2, 3]
 ax1.legend(
     [handles[idx] for idx in order],
     [labels[idx] for idx in order],
-    loc='upper left',
-    ncol=2,
+    loc='lower right',
+    # ncol=2,
 )
 
-for ax in [ax1, ax2]:
+handles, labels = ax2.get_legend_handles_labels()
+order = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 10, 11]
+ax2.legend(
+    [handles[idx] for idx in order],
+    [labels[idx] for idx in order],
+    loc='lower right',
+    # ncol=2,
+)
+
+handles, labels = ax3.get_legend_handles_labels()
+order = [3, 2, 1, 0, 4, 5]
+ax3.legend(
+    [handles[idx] for idx in order],
+    [labels[idx] for idx in order],
+    loc='lower right',
+    # ncol=2,
+)
+
+for ax in [ax1, ax2, ax3]:
+    locator = mdates.DayLocator([1])
+    formatter = mdates.ConciseDateFormatter(locator)
     ax.xaxis.set_major_locator(locator)
     ax.xaxis.set_major_formatter(formatter)
     ax.get_xaxis().get_major_formatter().show_offset = False
     ax.grid(True, linestyle=":", color='k')
 
 
-ax2.legend(loc='lower right')
 
 # Update the date in the HTML
 html_file = 'aus_vaccinations.html'
@@ -282,8 +365,10 @@ Path(html_file).write_text('\n'.join(html_lines) + '\n')
 
 fig1.savefig('cumulative_doses.svg')
 fig2.savefig('daily_doses_by_state.svg')
+fig3.savefig('utilisation.svg')
 
 fig1.savefig('cumulative_doses.png')
 fig2.savefig('daily_doses_by_state.png')
+fig3.savefig('utilisation.png')
 
 plt.show()
