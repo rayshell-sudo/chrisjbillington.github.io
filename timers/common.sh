acf95f31c7a1f4565988e85fa75790c8fc5471a6
chrisjbillington <chrisjbillington@gmail.com>
2021-08-29 13:19:44 +1000

    Use a git shallow clone to not fill up /tmp.

--- timers/common.sh
+++ timers/common.sh
@@ -19,15 +19,16 @@ function finish {
 trap finish EXIT
 
 # Sync with any remote changes. The way this works is that the main repo only ever pulls
-# from remote, our temporary clones are the ones pushing to remote. We would just clone
-# the entire repo from remote to a tempdir for each job, but it's actually very large so
-# we use the local one as an optimisation. The individual jobs can't just work on the
-# same git repo because that would create a collision. There is still a risk of
-# collision if two of them push at the same time, but it's much smaller since they all
-# issue a 'pull --rebase' immediately prior.
+# from remote, our temporary clones are the ones pushing to remote. We clone from the
+# local one to ensure we include any local (but committed) changes that might be being
+# tested. The individual jobs can't just work on the same git repo because that would
+# create a collision. There is still a risk of collision if two of them push at the same
+# time, but it's much smaller since they all issue a 'pull --rebase' immediately prior.
 git pull
 
-# clone, cd, and set remote
-git clone .git "$scratch"
+# clone, cd, and set remote. --depth 1 is important because the repository is huge, so
+# even though it's a local copy, we don't wan /tmp to fill up - it's a RAMdisk so can't
+# fit much.
+git clone --depth 1 .git "$scratch"
 cd "$scratch"
 git remote set-url origin git@github.com:chrisjbillington/chrisjbillington.github.io
