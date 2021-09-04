a660220dd919d0a55289b1443c31791377fa686e
chrisjbillington <chrisjbillington@gmail.com>
2021-09-04 14:57:28 +1000

    Delay some UK Pfizer, maybe we're only getting 300k initially

--- aus_vax.py
+++ aus_vax.py
@@ -253,11 +253,11 @@ AZ_local_supply_data = """
 #"""
 
 PFIZER_PROJECTED_SHIPMENTS= """ # In thousands per week
-2021-09-12 1000 + 200 + 1000 # Pfizer + Moderna + UK Pfizer doses. 4M from the UK over 4 weeks
+2021-09-12 1000 + 200 + 300 # Pfizer + Moderna + UK Pfizer doses. 4M from the UK over 4 weeks
 2021-09-19 1000 + 200 + 1000
 2021-09-26 1000 + 200 + 1000
 2021-10-03 2000 + 200 + 1000
-2021-10-10 2000 + 750 
+2021-10-10 2000 + 750 + 700
 2021-10-17 2000 + 750 
 2021-10-24 2000 + 750 
 2021-10-31 2000 + 750 
