49bd07ba89a4e7084b02d2bfba918e88af11a77d
chrisjbillington <chrisjbillington@gmail.com>
2021-09-25 08:41:36 +1000

    Increase VIC axis range

--- vic-2021.py
+++ vic-2021.py
@@ -882,7 +882,7 @@ if not LGA:
     if OLD:
         ymax = 10_000
     else:
-        ymax = 1_600
+        ymax = 2_400
     ax2.axis(ymin=0, ymax=ymax)
     ax2.yaxis.set_major_locator(mticker.MultipleLocator(ymax / 8))
     ax2.set_ylabel("Daily confirmed cases (linear scale)")
