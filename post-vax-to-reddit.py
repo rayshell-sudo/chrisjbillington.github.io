e7a6a4f727177c87fb9c083b6a12e9bd410c8fdd
chrisjbillington <chrisjbillington@gmail.com>
2021-11-06 12:12:41 +1100

    Simplify vax reddit posts

--- post-vax-to-reddit.py
+++ post-vax-to-reddit.py
@@ -42,7 +42,7 @@ IMAGES = [
     "coverage_by_agegroup.png",
     "coverage_2nd_by_agegroup.png",
     "coverage_by_state.png",
-    "coverage_2nd_by_state.png",    
+    "coverage_2nd_by_state.png",
     "cumulative_doses_longproject.png",
     "projection_cumulative_by_type.png",
 ]
@@ -52,42 +52,10 @@ this_script_url = (
     + "blob/master/post-vax-to-reddit.py"
 )
 
-# extra = """
-# Some changes in the projection.
-
-# 1. +500k doses from Singapore. Thanks, Singapore! These have been added to supply
-#    arriving by end of this week.
-
-# 2. I found some doses under the couch pillows: The 500k COVAX doses from a few months
-#    ago, I managed to accidentally misplace at some point. So I have added them back in.
-#    Also, I typod a 1M doses projected Pfizer shipment in early September as only 100k
-#    doses 😬. So I've bumped it back up to +1M. These two mistakes account for 1.4M doses.
-
-# Apologies for the mistakes. After correcting these and adding the Singapore doses, the
-# "dip" is all but gone. And the October spike is reduced, because at that point, we'll be
-# running out of people to vaccinate.
-# """
-
-# # Schedule some extra text:
-# if datetime.now().strftime('%Y-%m-%d') != '2021-09-01':
-extra = ""
-
 COMMENT_TEXT = f"""\
 More info/methodology: https://chrisbillington.net/aus_vaccinations.html
-{extra}
-FAQ:
 
-Q. Why did the projections change?
-
-A. These plots used to make projections based on expected supply, which was more
-   accurate than extrapolating from current dose rates when the rollout was
-   supply-limited. Now that the rollout is demand-limited, I've changed it to instead
-   project future doses based on a fit of recent first-doses to an exponential function
-   approaching some maximum level of uptake. Second doses are assumed to follow first
-   doses after the average dosing interval (i.e. how many days ago the first dose
-   coverage level was equal to the current second-dose coverage level).
-
-This post was made by a [bot]({this_script_url}) 🤖. Please let me know if something
+This post was made by [a bot]({this_script_url}) 🤖. Please let me know if something
 looks broken."""
 
 
