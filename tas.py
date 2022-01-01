cfbcd6f04849d1be73d29b152e3aac95af477a24
chrisjbillington <chrisjbillington@gmail.com>
2022-01-01 21:04:12 +1100

    TAS ymax

--- tas.py
+++ tas.py
@@ -417,7 +417,7 @@ if True:  # Just to keep the diff with nsw.py sensible here
     if VAX:
         ymax = 5_000
     else:
-        ymax = 400
+        ymax = 2000
     # if VAX:
     #     ymax = 40
     # else:
