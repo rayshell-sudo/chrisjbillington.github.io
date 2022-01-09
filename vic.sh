a58e91a199e9fbd7dfe03538b18a4dbd0e154880
chrisjbillington <chrisjbillington@gmail.com>
2022-01-10 09:26:47 +1030

    No VIC LGA updates for now

--- vic.sh
+++ vic.sh
@@ -3,8 +3,8 @@ set -e
 export MPLBACKEND=Agg
 python vic-2021.py
 python vic-2021.py vax
-for i in {0..9}
-do
-   python vic-2021.py $i
-done
+# for i in {0..9}
+# do
+#    python vic-2021.py $i
+# done
 # python vic_vax.py
