ff1584026f89364fc422a4d8247c446b4e9e1956
chrisjbillington <chrisjbillington@gmail.com>
2022-01-25 11:28:11 +1100

    NZ ymax

--- nz.py
+++ nz.py
@@ -707,9 +707,9 @@ if not (AUCKLAND or NOTAUCKLAND):
     if OLD and dates[-1] < np.datetime64('2022-01-24'):
         ymax = 400
     elif OLD:
-        ymax = 40_000
+        ymax = 1_000
     elif VAX:
-        ymax = 40_000
+        ymax = 1_000
     else:
         ymax = 400
 
